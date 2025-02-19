e94a7c36f9dbcf8480f27f2e8747bdb395a6aa4b
landonkahl <landonkahl@localhost>
2025-02-19 17:18:27 -0600

    Studio 4: Nationalism

--- src/studio4/Flag.java
+++ src/studio4/Flag.java
@@ -6,6 +6,29 @@ import edu.princeton.cs.introcs.StdDraw;
 
 public class Flag {
 	public static void main(String[] args) {
+		StdDraw.setPenColor(Color.YELLOW);
+		StdDraw.filledRectangle(0.5, 0.25, .2, 1);
 		
+		StdDraw.setPenColor(Color.RED);
+		StdDraw.filledRectangle(0.5, 0.5, 0.05, 1);
+		
+		StdDraw.setPenColor(Color.BLUE);
+		StdDraw.filledCircle(.5, .5, .2);
+		
+		StdDraw.setPenColor(Color.YELLOW);
+		StdDraw.filledRectangle(0.2, 0.15, 1, 0.05);
+		StdDraw.filledRectangle(0.2, 0.85, 1, 0.05);
+		
+		StdDraw.setPenColor(Color.RED);
+		StdDraw.filledRectangle(0.85, 0.25, 0.05, 1);
+		StdDraw.filledRectangle(0.15, 0.25, 0.05, 1);
+		
+		StdDraw.setPenColor(Color.WHITE);
+		StdDraw.filledEllipse(0.5, 0.5, 0.2, 0.05);
+		StdDraw.setPenColor(Color.BLACK);
+		StdDraw.filledCircle(0.5, 0.5, 0.025);
+		
+		StdDraw.setPenRadius(0.05);
+		StdDraw.rectangle(0.5, 0.5, 0.5, 0.5);
 	}
 }
--- src/studio4/InterpretDrawingFile.java
+++ src/studio4/InterpretDrawingFile.java
@@ -1,5 +1,6 @@
 package studio4;
 
+import java.awt.Color;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
@@ -19,6 +20,47 @@ public class InterpretDrawingFile {
 		chooser.showOpenDialog(null);
 		File f = new File(chooser.getSelectedFile().getPath());
 		Scanner in = new Scanner(f); //making Scanner with a File
+		
+		String shape = in.next();
+		int redness = in.nextInt();
+		int greenness = in.nextInt();
+		int blueness = in.nextInt();
+		boolean filled = in.nextBoolean();
+		
+		
+		
+		Color userColor = new Color(redness, greenness, blueness);
+		StdDraw.setPenColor(userColor);
+		
+		if (shape == "rectangle") {
+			double x = in.nextInt();
+			double y = in.nextInt();
+			double halfWidth = in.nextInt();
+			double halfHeight = in.nextInt();
+			
+			if (filled == false) {
+				StdDraw.shape();
+			}
+			else if (filled == true) {
+				StdDraw.("fillled" + shape)();
+			}
+		}
+		else if (shape == "ellipse") {
+			double x = in.nextInt();
+			double y = in.nextInt();
+			double semiMajorAxis = in.nextInt();
+			double semiMinorAxis = in.nextInt();
+		}
+		else if (shape == "triangle") {
+			double x1 = in.nextInt();
+			double y1 = in.nextInt();
+			double x2 = in.nextInt();
+			double y2 = in.nextInt();
+			double x3 = in.nextInt();
+			double y3 = in.nextInt();
+		}
+		
+		
 		
 	}
 }
